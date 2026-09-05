876f75b868fa3acc4ee4539cb1c4d32a9f118787
Gnaneshwar Bandari <31314778+gnaneshwarbandari@users.noreply.github.com>
2026-09-05 15:53:45 +0530

    final email

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@
     const pageLoadTime = Date.now();
 
     // Replace with your copied Google Web App URL
-    const GOOGLE_SCRIPT_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbw2Asckw79JPcoH0kqfvfas_sOGDiRcD-tzFTc6eDOm9lEG-hGrWNUr0vc1SguurF3d/exec";
+    const GOOGLE_SCRIPT_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzbvEn1nWO0GBaWz-anEc9HHlVaPxiUrxGWNWWgAoAPUYjz0xSzwq7fCV0mxCttVL1EkQ/exec";
 
     function renderQuoteCaptcha() {
         const canvas = document.getElementById("quoteCaptchaCanvas");
